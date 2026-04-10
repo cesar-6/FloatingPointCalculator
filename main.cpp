fdd8e6015eb090386d12205f5b51e6a5a47cec4c
cesar-6 <FuentesHernandez.C173944@stu.sanjac.edu>
2026-04-10 13:49:11 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+=========================================================
+main.cpp
+Programming Assignment #2
+Floating-Point Calculator (MIPS Implementation)
+Author: Cesar Fuentes
+=========================================================
+
+#include "Calculator.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Floating-Point Calculator implemented in MIPS Assembly.\n";
+    std::cout << "See FloatingPointCalculator.asm for full logic.\n";
+    return 0;
+}
