801e57f8708ee72925f0dac8015c3f205b1a549a
cesar-6 <FuentesHernandez.C173944@stu.sanjac.edu>
2026-04-10 13:49:39 -0500

    Create Calculator.h

---
+++ Calculator.h
@@ -0,0 +1,13 @@
+=========================================================
+Calculator.h
+Programming Assignment #2
+Floating-Point Calculator (MIPS Implementation)
+Author: Cesar Fuentes
+=========================================================
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+void runCalculator(); // placeholder
+
+#endif
